5634b2e4926337fd105e61503b2cdbe0cdf34b2c
Narendra Nag <narendranag@gmail.com>
2024-11-05 15:17:33 -0600

    Detect attached device

--- nfc.py
+++ nfc.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from nfc-reader-writer!")
+import nfc
 
+def device_found(target):
+	print(f"Device detected {target}")
+	return True
+
+def main():
+    with nfc.ContactlessFrontend('usb') as clf:
+		if clf:
+			print("NFC reader/writer connected.")
+			clf.connect(rdwr={'on-connect': device_found})
+		else:
+	    	print("No NFC device found")
 
 if __name__ == "__main__":
     main()
